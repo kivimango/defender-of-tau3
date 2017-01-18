1a79636ca1d9d3a6c977d4d769bd245f6b346467
kivimango <dev@c0d3r.nhely.hu>
2017-01-18 18:03:32 +0100

    Initial commit

---
+++ src/com/kivimango/spacefighter/Game.java
@@ -0,0 +1,30 @@
+package com.kivimango.spacefighter;
+
+import java.awt.Canvas;
+
+/**
+ * Implementation of a 2D Space Fighter Game in JAVA.The code is based on RealTutsGML's 'Java Game Development' 
+ * series on Youtube.
+ * 
+ * Game Mechanism : The player can control a spaceship via keyboard to shoot enemies down.
+ * 
+ * @author RealTutsGML
+ * @link https://www.youtube.com/channel/UCOs7Q7IeuzgRyARaEqif75A
+ * @see <a href="https://www.youtube.com/watch?v=hXImR8Wm53M">Java Game Development #1 - Starting up our Project</a>
+ */
+
+public class Game extends Canvas implements Runnable {
+
+	private static final long serialVersionUID = -7654654228944009721L;
+
+
+	@Override
+	public void run() {
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
